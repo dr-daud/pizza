1c67e4c73c125ee2dd0f5769ce59b7b10deda047
Daud <daud_edilbiev@mail.ru>
2025-02-28 11:52:48 +0400

    axios

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useContext } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
 
 import PizzaBlock from '../components/pizza-block/pizza-block';
 import Skeleton from '../components/skeleton/skeleton';
@@ -7,28 +8,29 @@ import Categories from '../components/categories/categories';
 import Sort from '../components/sort/sort';
 import Pagination from '../components/pagination/pagination';
 import { SearchContext } from '../app/app';
+import { setCurrentPage } from '../redux/slices/filterSlice';
 
 const Home = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [currentPage, setCurrentPage] = useState(1);
     const { searchValue } = useContext(SearchContext);
 
     const categoryId = useSelector(state => state.filter.categoryId);
-    const sort = useSelector(state => state.filter.sort)
+    const sort = useSelector(state => state.filter.sort);
+    const currentPage = useSelector(state => state.filter.currentPage);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         setIsLoading(true)
 
         const sortBy = sort.sortProperty.replace('-', '');
         const order = sort.sortProperty.includes('-') ? 'desc' : 'asc';
-        const category = categoryId > 0 ? `category=${categoryId}` : '';
+        const category = categoryId > 0 ? `&category=${categoryId}` : '';
         const search = searchValue ? `&search=${searchValue}` : '';
 
-        fetch(`https://67af4195dffcd88a6786195f.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`)
-            .then(res => res.json())
-            .then(json => {
-                setItems(json)
+        axios.get(`https://67af4195dffcd88a6786195f.mockapi.io/items?page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`)
+            .then(res => {
+                setItems(res.data)
                 setIsLoading(false)
             })
         window.scrollTo(0, 0)
@@ -44,9 +46,9 @@ const Home = () => {
             <div className="content__items">
                 {isLoading
                     ? [...new Array(10)].map((_, index) => <Skeleton key={index} />)
-                    : items.map(obj => (<PizzaBlock key={obj.id} {...obj} />))}
+                    : items.map ? items.map(obj => (<PizzaBlock key={obj.id} {...obj} />)) : null}
             </div>
-            <Pagination onChangePage={(number) => setCurrentPage(number)} />
+            <Pagination onChangePage={(number) => dispatch(setCurrentPage(number))} />
         </div>
     )
 }
